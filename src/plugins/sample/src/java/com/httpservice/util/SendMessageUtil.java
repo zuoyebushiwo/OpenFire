3e797d477dab3a189e6db66fc9a66b37016ed103
zuoyebushiwo <957168943@qq.com>
2015-03-17 21:01:16 +0800

    Change-Id: I18a56c958b0bb22b448fa9a5204f78c91657c2e2

--- src/plugins/sample/src/java/com/httpservice/util/SendMessageUtil.java
+++ src/plugins/sample/src/java/com/httpservice/util/SendMessageUtil.java
@@ -1,9 +1,24 @@
 package com.httpservice.util;
 
+import net.sf.json.JSONObject;
+
+import org.jivesoftware.openfire.RoutingTable;
+
 /**
  * @author ZhangXueJun
  * @date 2015年3月17日
  */
 public class SendMessageUtil {
+
+	public void sendSingleMessage(JSONObject object, RoutingTable routingTable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void sendSingleMessage(org.json.JSONObject object,
+			RoutingTable routingTable) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
