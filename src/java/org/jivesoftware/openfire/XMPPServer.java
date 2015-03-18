7251eb972a09ba77d7f5a9b6fd7a584c290b72ad
zuoyebushiwo <957168943@qq.com>
2015-03-18 10:50:39 +0800

    Change-Id: I17aec30b199c9a1acefc92f6edb5b393c5834197

--- src/java/org/jivesoftware/openfire/XMPPServer.java
+++ src/java/org/jivesoftware/openfire/XMPPServer.java
@@ -115,6 +115,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xmpp.packet.JID;
 
+import com.test.plugin.iqhandle.TestIQHandle;
+
 /**
  * The main XMPP server that will load, initialize and start all the server's
  * modules. The server is unique in the JVM and could be obtained by using the
@@ -577,10 +579,10 @@ public class XMPPServer {
         loadModule(MultiUserChatManager.class.getName());
         loadModule(ClearspaceManager.class.getName());
         loadModule(IQMessageCarbonsHandler.class.getName());
-//        loadModule(TestIQHandle.class.getName());
         // Load this module always last since we don't want to start listening for clients
         // before the rest of the modules have been started
         loadModule(ConnectionManagerImpl.class.getName());
+//        loadModule(TestIQHandle.class.getName());
         
         // Keep a reference to the internal component manager
         componentManager = getComponentManager();
